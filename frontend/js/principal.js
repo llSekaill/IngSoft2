5b88d140e5d19a060fc187b082d1140f33828399
llSekaill <20151102@aloe.ulima.edu.pe>
2021-10-06 17:14:21 -0500

    Ingresando frontend y backend actualizado

--- frontend/js/principal.js
+++ frontend/js/principal.js
@@ -30,7 +30,7 @@ const butRegistrar = () => {
             }
         }).then((respRegistrar) => {
             console.log(respRegistrar.statusText);
-            if(respRegistrar.statusText == "OK"){             
+            if(respRegistrar.statusText == "OK"){           
                 var divAlerta = document.createElement('div');
                 divAlerta.setAttribute("class","alert alert-success");
                 divAlerta.setAttribute("id","alerta1");
@@ -53,17 +53,14 @@ const butRegistrar = () => {
 
 const butAcceder = () => {
     document.getElementById('alerta2').remove()
-
     const logusuario = document.getElementById('logusuario').value;
     const logpassword = document.getElementById('logpassword').value;
     if(logusuario == "" && logpassword == ""){
-        
         var divAlerta = document.createElement('div');
         divAlerta.setAttribute("class","alert alert-danger");
         divAlerta.setAttribute("id","alerta2");
         divAlerta.innerHTML = "Ingrese su usuario y contraseña";
         document.getElementById('mensAlerta2').appendChild(divAlerta);
-
     }else{
         if(logpassword == ""){
             var divAlerta = document.createElement('div');
@@ -78,51 +75,112 @@ const butAcceder = () => {
                 divAlerta.setAttribute("id","alerta2");
                 divAlerta.innerHTML = "Ingrese primero su usuario";
                 document.getElementById('mensAlerta2').appendChild(divAlerta);
-
             }else{
                 const body = {
                     usuario : logusuario,
                     password : logpassword
-                    }
-    
-                    fetch(URL_BASE+"/loginpartLider",
-                    {
-                        method : 'POST',
-                        body : JSON.stringify(body),
-                        headers : {
-                            "Content-type" : "application/json"
-                        },
-                        
-                    }).then((respLogin) => {
-                        respLogin.json().then((data) => {
-                            if(data.msg == "Error"){
-                                console.log("Datos no registrado");
-                                var divAlerta = document.createElement('div');
-                                divAlerta.setAttribute("class","alert alert-danger");
-                                divAlerta.setAttribute("id","alerta2");
-                                divAlerta.innerHTML = "Usuario o contraseña incorrecto";
-                                document.getElementById('mensAlerta2').appendChild(divAlerta);
-                            }else{
-                                const login1 = document.getElementById('login1');   
-                                const login2 = document.getElementById('login2');
-                                const butcerrar = document.getElementById('butX');
-                                login1.setAttribute("style","visibility:hidden;");
-                                login2.removeAttribute("style","visibility:hidden;");
-                                login2.firstElementChild.innerHTML = data.usuario;
-                                var divAlerta = document.createElement('div');
-                                divAlerta.setAttribute("id","alerta2");
-                                document.getElementById('mensAlerta2').appendChild(divAlerta);
-                                butcerrar.click();
-                                console.log(document.getElementById('alerta2'));
+                }
+                fetch(URL_BASE+"/loginpartLider",
+                {
+                    method : 'POST',
+                    body : JSON.stringify(body),
+                    headers : {
+                        "Content-type" : "application/json"
+                    },
+                }).then((respLogin) => {
+                    respLogin.json().then((data) => {
+                        if(data.msg != "Error"){
+                            const login1 = document.getElementById('login1');   
+                            const login2 = document.getElementById('login2');
+                            const butcerrar = document.getElementById('butX');
+                            login1.setAttribute("style","visibility:hidden;");
+                            login2.removeAttribute("style","visibility:hidden;");
+                            login2.firstElementChild.innerHTML = data.usuario;
+                            
+                            /*const lilogin1 = document.createElement('li');
+                            const lilogin2 = document.createElement('li');
+                            const lilogin3 = document.createElement('li');
+                            const lilogin4 = document.createElement('li');
+                            const lilogin5 = document.createElement('li');
+                            const a1 = document.createElement('a');
+                            const a2 = document.createElement('a');
+                            const a3 = document.createElement('a');
+                            const hr = document.createElement('hr');
+                            const a4 = document.createElement('a');
+                            a1.setAttribute("class","dropdown-item link");
+                            a2.setAttribute("href","perfil.html");
+                            a2.setAttribute("class","dropdown-item link");
+                            a3.setAttribute("class","dropdown-item link");
+                            hr.setAttribute("class","dropdown-divider");
+                            a4.setAttribute("id","cerrarSec");
+                            a4.setAttribute("class","dropdown-item link active");
+                            a1.innerHTML = "Torneo";
+                            a2.innerHTML = "Mi Perfil";
+                            a3.innerHTML = "Mi Equipo";
+                            a4.innerHTML = "Cerrar Sesión";
+                            lilogin1.appendChild(a1);
+                            lilogin2.appendChild(a2);
+                            lilogin3.appendChild(a3);
+                            lilogin4.appendChild(hr);
+                            lilogin5.appendChild(a4);
+                            document.getElementById('ullogin').appendChild(lilogin1);
+                            document.getElementById('ullogin').appendChild(lilogin2);
+                            document.getElementById('ullogin').appendChild(lilogin3);
+                            document.getElementById('ullogin').appendChild(lilogin4);
+                            document.getElementById('ullogin').appendChild(lilogin5);
+                            */
+                            var divAlerta = document.createElement('div');
+                            divAlerta.setAttribute("id","alerta2");
+                            document.getElementById('mensAlerta2').appendChild(divAlerta);
+                            butcerrar.click();
+                        }else{ 
+                            const bodyA = {
+                                adminname : logusuario,
+                                adminpass : logpassword
                             }
-                        })
-                    }).catch((error) => {
-                        console.error(error);
-                        console.log(error);
-                    });
+                            fetch(URL_BASE+"/loginAdmin",
+                            {
+                                method : 'POST',
+                                body : JSON.stringify(bodyA),
+                                headers : {
+                                    "Content-type" : "application/json"
+                                },
+                            }).then((respLoginA) => {
+                                respLoginA.json().then((dataA) => {
+                                    console.log(dataA);
+                                    if(dataA.msg != "Error"){
+                                        const login1 = document.getElementById('login1');   
+                                        const login2 = document.getElementById('login2');
+                                        const butcerrar = document.getElementById('butX');
+                                        login1.setAttribute("style","visibility:hidden;");
+                                        login2.removeAttribute("style","visibility:hidden;");
+                                        login2.firstElementChild.innerHTML = dataA.adminname;
+                                        var divAlerta = document.createElement('div');
+                                        divAlerta.setAttribute("id","alerta2");
+                                        document.getElementById('mensAlerta2').appendChild(divAlerta);
+                                        butcerrar.click();
+                                        console.log(document.getElementById('alerta2'));
+                                    }else{
+                                        console.log("Datos no registrado");
+                                        var divAlerta = document.createElement('div');
+                                        divAlerta.setAttribute("class","alert alert-danger");
+                                        divAlerta.setAttribute("id","alerta2");
+                                        divAlerta.innerHTML = "Usuario o contraseña incorrecto";
+                                        document.getElementById('mensAlerta2').appendChild(divAlerta);
+                                    }
+                                })
+                            }).catch((error) => {
+                                console.error(error);
+                                console.log(error);
+                            });
+                        }
+                    })
+                }).catch((error) => {
+                    console.error(error);
+                    console.log(error);
+                });
             }
         }
-
     }
 }
 
@@ -134,7 +192,6 @@ var butRegis = (event) => {
     document.getElementById('menAlerta1').appendChild(div);
     document.getElementById('logusuario').value = "";
     document.getElementById('logpassword').value = "";
-
 }
 
 var butRegregar = (event) => {
@@ -142,16 +199,20 @@ var butRegregar = (event) => {
     var div = document.createElement('div');
     div.setAttribute("id","alerta2");
     document.getElementById('mensAlerta2').appendChild(div);
-    
 }
 
+const cerrarSec = (event) => {
+    console.log("Funciona cerrar seccion");
+    const login1 = document.getElementById('login1');   
+    const login2 = document.getElementById('login2');
+    login2.setAttribute("style","visibility:hidden;");
+    login1.removeAttribute("style","visibility:hidden;");
+}
 var main = () => {
-
     document.querySelector("#butRegistrar").addEventListener("click", butRegistrar);
     document.querySelector("#butAcceder").addEventListener("click", butAcceder);
     document.querySelector("#butRegistrate").addEventListener("click", butRegis);
     document.querySelector("#butRegresar").addEventListener("click", butRegregar);
-    
-    
+    document.querySelector("#cerrarSec").addEventListener("click", cerrarSec);
 }
 window.addEventListener("load", main);
